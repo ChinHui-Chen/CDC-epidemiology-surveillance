9babecbebda54cc24a75a6e6a60dad0b8f2d9135
Dongo Chen <dongogo@gmail.com>
2014-12-10 16:34:00 +0800

    Update import script.

--- data/script/import.sh
+++ data/script/import.sh
@@ -1,1 +1,1 @@
-mongoimport -h localhost:3001 --db meteor --collection articles --file ./data/demo.json --fields DiseaseName,Relevance,Source,Title,Snippet,Url,Language,Location,Lat,Lng,PublishTime,CrawlTime
+mongoimport -h localhost:3001 --db meteor --collection articles --file ../demo2.json --fields DiseaseName,Relevance,Source,Title,Snippet,Url,Language,Location,Lat,Lng,PublishTime,CrawlTime
