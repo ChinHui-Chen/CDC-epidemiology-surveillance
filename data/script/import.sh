e7b2ac086976f27ed117018efa0dac6550ebf145
ChinHui-Chen <dongogo@gmail.com>
2016-03-09 02:40:16 +0800

    update mongo import script

--- data/script/import.sh
+++ data/script/import.sh
@@ -1,1 +1,2 @@
-mongoimport -h localhost:3001 --db meteor --collection articles --file ../demo2.json --fields DiseaseName,Relevance,Source,Title,Snippet,Url,Language,Location,Lat,Lng,PublishTime,CrawlTime
+#mongoimport -h localhost:3001 --db meteor --collection articles --file ../demo3.json --fields DiseaseName,Relevance,Source,Title,Snippet,Url,Language,Location,Lat,Lng,PublishTime,CrawlTime
+mongoimport -h localhost:3001 --db meteor --collection articles --file ../demo2.json
